2258316514c81e2961358cdcd993c966ecef2451
Samadhan Mane <samadhanmane2324@gmail.com>
2025-07-18 10:19:00 +0530

    Added two factor authentication changes and asset updates

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,37 +1,17 @@
-import logo from './logo.svg'
-import dropdown_icon from './dropdown_icon.svg'
-import menu_icon from './menu_icon.svg'
-import cross_icon from './cross_icon.png'
-import chats_icon from './chats_icon.svg'
-import verified_icon from './verified_icon.svg'
-import arrow_icon from './arrow_icon.svg'
-import info_icon from './info_icon.svg'
-import upload_icon from './userIcon.png'
+import upload_icon from './usericon.png'
 import header_hall from './header_hall.png'
 import High_Capacity_img from './High_Capacity_img.jpg'
 import mitaoe from './mitaoe.jpeg'
-import headerlogo from './headerlogo.svg'
 import mitaoe_logo from './MITAOE-logo.png'
 import Low_Capacity from './Low_Capacity.svg'
 import High_Capacity from './High_Capacity.svg'
 import FacilityBooking from './FacilityBooking.png'
 
-
-
 export const assets = {
-    logo,
-    chats_icon,
-    verified_icon,
-    info_icon,
-    menu_icon,
-    cross_icon,
-    dropdown_icon,
     upload_icon,
-    arrow_icon,
     header_hall,
     High_Capacity_img,
     mitaoe,
-    headerlogo,
     mitaoe_logo,
     FacilityBooking
 }
